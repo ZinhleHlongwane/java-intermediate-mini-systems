303364fb1fd55c79b959535664d83ce189d956b3
Zinhle Hlongwane <zinhlehlongwani56@gmail.com>
2026-05-14 04:05:51 +0200

    Build basic blueprint in Task.java

--- src/main/java/com/zinhle/taskmanager/models/Task.java
+++ src/main/java/com/zinhle/taskmanager/models/Task.java
@@ -2,5 +2,21 @@ package com.zinhle.taskmanager.models;
 
 public class Task {
     
-    
+    private String title;
+    private Priority priority;
+    private Status status;
+
+    public Task(String title, Priority priority, Status status) {
+        this.title = title;
+        this.priority = priority;
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
